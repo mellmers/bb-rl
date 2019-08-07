b48b2e91178d3567208c3b67093c038c813cdfd1
mellmers <moritz.ellmers@web.de>
2019-08-08 01:03:20 +0200

    new partner view

--- src/components/Partner/Partner.js
+++ src/components/Partner/Partner.js
@@ -4,9 +4,9 @@ import {injectIntl} from "react-intl";
 
 import Footer from './../Footer/Footer';
 
-import MMOGA_IMG from './img/Twitch_Support_MMOGA_DE.png';
-import Runtime_IMG from './img/Logo_Runtime_GG.png';
-import Merch_IMG from './img/Merch-Shop-Logo.png';
+import MMOGA_IMG from './img/MMOGA.png';
+import Teamspeak_IMG from './img/Teamspeak.png';
+import Spreadshirt_IMG from './img/Spreadshirt.png';
 
 import './Partner.css';
 
@@ -18,32 +18,51 @@ class Partner extends React.PureComponent {
             <div className="section partner">
                 <div className="container">
                     <div className="inner">
-                        <h2 className="text-center">{formatMessage(messages.partnerHead)}</h2>
+                        <h2 className="text-center highlighted-text">{formatMessage(messages.partnerHead)}</h2>
 
                         <div className="row">
                             <div className="col-12 col-sm-6 col-md-4">
-                                <a href="https://www.mmoga.de/?ref=29428" target="_blank" rel="noopener noreferrer"><img src={MMOGA_IMG} alt="MMOGA"/></a>
-                                <p>
-                                    <strong>{formatMessage(messages.partnerMmogaHead)}</strong>
-                                </p>
-                                <p>
-                                    {formatMessage(messages.partnerMmogaContent)}
-                                </p>
+                                <a href="https://www.mmoga.de/?ref=29428" target="_blank" rel="noopener noreferrer" className="partner-card">
+                                    <img src={MMOGA_IMG} />
+                                    <div className="divider"/>
+                                    <ul>
+                                        <li>Gamekeys</li>
+                                        <li>Faire Preise</li>
+                                        <li>Schneller Versand</li>
+                                        <li>Riesen Auswahl</li>
+                                        <li>Poe Items</li>
+                                        <li>Gamescards</li>
+                                    </ul>
+                                </a>
                             </div>
-                            <div className="col-12 col-sm-6 col-md-4 runtime">
-                                <a href="https://runtime.idevaffiliate.com/780-1-3-2.html" target="_blank" rel="noopener noreferrer"><img src={Runtime_IMG} alt="Runtime.gg"/></a>
-                                <p>
-                                    <strong>{formatMessage(messages.partnerRuntimeHead)}</strong>
-                                </p>
-                                <p>
-                                    {formatMessage(messages.partnerRuntimeContent)}
-                                </p>
+                            <div className="col-12 col-sm-6 col-md-4">
+                                <a href="https://runtime.idevaffiliate.com/780-1-3-2.html" target="_blank" rel="noopener noreferrer" className="partner-card">
+                                    <img src={Teamspeak_IMG} />
+                                    <div className="divider"/>
+                                    <ul>
+                                        <li>Beste Sprachqualität</li>
+                                        <li>Minimale CPU Auslastung</li>
+                                        <li>Erweiteres Rechtesystem</li>
+                                        <li>Offline / LAN-Funktion</li>
+                                        <li>Anonyme Nutzung</li>
+                                        <li>Spam Frei</li>
+                                    </ul>
+                                </a>
                             </div>
-                            <div className="col-12 col-sm-6 col-md-4 merch-shop">
-                                <a href="https://shop.spreadshirt.de/Battleground-Bulls/" target="_blank" rel="noopener noreferrer"><img src={Merch_IMG} alt="Merch-Shop"/><h2>{formatMessage(messages.partnerMerchHead)}</h2></a>
-                                <p>
-                                    <strong>{formatMessage(messages.partnerMerchContent)} <a className="link-light" href="https://shop.spreadshirt.de/Battleground-Bulls/" target="_blank" rel="noopener noreferrer">{formatMessage(messages.partnerMerchHead)}</a>!</strong>
-                                </p>
+                            <div className="col-12 col-sm-6 col-md-4">
+                                <a href="https://shop.spreadshirt.de/Battleground-Bulls/" target="_blank" rel="noopener noreferrer" className="partner-card">
+                                    <img src={Spreadshirt_IMG} />
+                                    <div className="divider"/>
+                                    <ul>
+                                        <li>T-Shirt's</li>
+                                        <li>Pullover & Hoodies</li>
+                                        <li>Tank Tops</li>
+                                        <li>Accessoires</li>
+                                        <li>Hüllen</li>
+                                        <li>Wandbilder</li>
+                                        <li>Selber gestalten</li>
+                                    </ul>
+                                </a>
                             </div>
                         </div>
                     </div>
